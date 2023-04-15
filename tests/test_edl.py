b3cc9c4dfe9cacfaab5c338b58f44e993ace8237
Teresa LaBolle <teresalabolle@gmail.com>
2023-04-15 15:32:02 -0700

    Wrote unit test for _parse_packet throwing 'invalid FECF' exception.

--- tests/test_edl.py
+++ tests/test_edl.py
@@ -1,6 +1,6 @@
 import unittest
 
-from oresat_c3.protocols.edl import EdlServer, EdlClient, EdlError
+from oresat_c3.protocols.edl import EdlServer, EdlClient, EdlError, crc16_bytes
 
 
 class TestEdl(unittest.TestCase):
@@ -35,3 +35,45 @@ class TestEdl(unittest.TestCase):
         #Test if EdlError "EDL packet too short" exception is thrown
         with self.assertRaisesRegex(EdlError, f'EDL packet too short: {server._TC_MIN_LEN - 1}'):
             server._parse_packet(short_packet, src_dest = 0) #Using 0 as a dummy src_dest value
+
+    def test_invalid_fecf(self):
+
+       hmac_key = b'\x00' * 128
+       seq_num = 0
+       payload = b'\x12\x34\x56'
+
+       #Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
+       server = EdlServer(hmac_key, seq_num)
+       client = EdlClient(hmac_key, seq_num)
+
+       #Generating a valid request ticket
+       req_packet = client.generate_request(payload)
+
+       #Modifying FECF so that it is Invalid
+       invalid_fecf_packet = req_packet[:-2] + b'\xFF\XFF'
+
+       #Checking if EdlError exception is raised for the invalid FECF
+       crc16_raw = invalid_fecf_packet[-2:]
+       crc16_raw_calc = crc16_bytes(invalid_fecf_packet[:-2])
+       with self.assertRaisesRegex(EdlError, f'invalid FECF: .* vs .*'):
+        server._parse_packet(invalid_fecf_packet, src_dest = 0)
+
+#     def test_invalid_packet_or_frame_len(self):
+#        hmac_key = b'\x00' * 128
+#        seq_num = 0
+#        payload = b'\x12\x34\x56'
+#
+#        #Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
+#        server = EdlServer(hmac_key, seq_num)
+#        client = EdlClient(hmac_key, seq_num)
+#
+#        #Generating a valid request packet
+#        req_packet = client.generate_request(payload)
+#
+#        #Modifying the packet length in the primary header so that it's invalid
+#        invalid_packet_len = 255
+#        invalid_packet = req_packet[:4] + invalid_packet_len.to_bytes(2, 'big') + req_packet[6:]
+#
+#        #Checking if EdlError is raised for invalid packet or fram length
+#        with self.assertRaisesRegex(EdlError, 'USLP invalid packet or frame length'):
+#         server._parse_packet(invalid_packet, src_dest = 0)

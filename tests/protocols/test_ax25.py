1a96ab494d95ee237ef4cfc4e76bd3cc753b5fbd
ryanpdx <rmedick@protonmail.com>
2023-05-29 10:00:42 -0700

    minor cleanup of ax25 and edl unit tests

--- tests/protocols/test_ax25.py
+++ tests/protocols/test_ax25.py
@@ -2,10 +2,12 @@ import unittest
 
 from oresat_c3.protocols.ax25 import generate_ax25_packet, AX25_PAYLOAD_MAX_LEN, Ax25Error
 
+
 class TestAx25(unittest.TestCase):
 
     def test_generate_ax25_invalid_dest_callsign_length(self):
-        #Set destination callsign with a length greater than AX25_CALLSIGN_LEN
+        '''Set destination callsign with a length greater than AX25_CALLSIGN_LEN'''
+
         invalid_dest_callsign = 'INVAID_CALLSIGN'
         dest_ssid = 1
         src_callsign = 'SRC'
@@ -16,10 +18,19 @@ class TestAx25(unittest.TestCase):
 
         # Check if Ax25Error is raised for the invalid destination callsign length
         with self.assertRaises(Ax25Error):
-            generate_ax25_packet(invalid_dest_callsign, dest_ssid, src_callsign, src_ssid, control, pid, payload)
+            generate_ax25_packet(
+                invalid_dest_callsign,
+                dest_ssid,
+                src_callsign,
+                src_ssid,
+                control,
+                pid,
+                payload
+            )
 
     def test_generate_ax25_invalid_dest_ssid_large(self):
-        #Set destination ssid greater than 15
+        '''Set destination ssid greater than 15'''
+
         dest_callsign = 'DEST'
         invalid_dest_ssid = 16
         src_callsign = 'SRC'
@@ -29,11 +40,20 @@ class TestAx25(unittest.TestCase):
         payload = b'\x01\x02\x03'
 
         # Check if Ax25Error is raised for the invalid destination ssid value
-        with self.assertRaisesRegex(Ax25Error, 'dest_ssid must be between 0 and 15'):
-            generate_ax25_packet(dest_callsign, invalid_dest_ssid, src_callsign, src_ssid, control, pid, payload)
+        with self.assertRaises(Ax25Error):
+            generate_ax25_packet(
+                dest_callsign,
+                invalid_dest_ssid,
+                src_callsign,
+                src_ssid,
+                control,
+                pid,
+                payload
+            )
 
     def test_generate_ax25_invalid_dest_ssid_small(self):
-        #Set destination ssid less than 0
+        '''Set destination ssid less than 0'''
+
         dest_callsign = 'DEST'
         invalid_dest_ssid = -1
         src_callsign = 'SRC'
@@ -43,11 +63,20 @@ class TestAx25(unittest.TestCase):
         payload = b'\x01\x02\x03'
 
         # Check if Ax25Error is raised for the invalid destination ssid value
-        with self.assertRaisesRegex(Ax25Error, 'dest_ssid must be between 0 and 15'):
-            generate_ax25_packet(dest_callsign, invalid_dest_ssid, src_callsign, src_ssid, control, pid, payload)
+        with self.assertRaises(Ax25Error):
+            generate_ax25_packet(
+                dest_callsign,
+                invalid_dest_ssid,
+                src_callsign,
+                src_ssid,
+                control,
+                pid,
+                payload
+            )
 
     def test_generate_ax25_invalid_src_callsign(self):
-        #Set source callsign with a length greater than AX25_CALLSIGN_LEN
+        '''Set source callsign with a length greater than AX25_CALLSIGN_LEN'''
+
         dest_callsign = 'DEST'
         dest_ssid = 1
         invalid_src_callsign = 'INVALID_CALLSIGN'
@@ -57,11 +86,20 @@ class TestAx25(unittest.TestCase):
         payload = b'\x01\x02\x03'
 
         # Check if Ax25Error is raised for the invalid source callsign length
-        with self.assertRaisesRegex(Ax25Error, 'src callsign must be less than 6 chars'):
-            generate_ax25_packet(dest_callsign, dest_ssid, invalid_src_callsign, src_ssid, control, pid, payload)
+        with self.assertRaises(Ax25Error):
+            generate_ax25_packet(
+                dest_callsign,
+                dest_ssid,
+                invalid_src_callsign,
+                src_ssid,
+                control,
+                pid,
+                payload
+            )
 
     def test_generate_ax25_invalid_src_ssid_larger(self):
-        #Set source ssid to be greater than 15
+        '''Set source ssid to be greater than 15'''
+
         dest_callsign = 'DEST'
         dest_ssid = 1
         src_callsign = 'SRC'
@@ -71,11 +109,19 @@ class TestAx25(unittest.TestCase):
         payload = b'\x01\x02\x03'
 
         # Check if Ax25Error is raised for the invalid invalid source ssid value
-        with self.assertRaisesRegex(Ax25Error, 'src_ssid must be between 0 and 15'):
-            generate_ax25_packet(dest_callsign, dest_ssid, src_callsign, invalid_src_ssid, control, pid, payload)
+        with self.assertRaises(Ax25Error):
+            generate_ax25_packet(
+                dest_callsign,
+                dest_ssid,
+                src_callsign,
+                invalid_src_ssid,
+                control,
+                pid,
+                payload
+            )
 
     def test_generate_ax25_invalid_src_ssid_small(self):
-        #Set source ssid with to be less than 0
+        '''Set source ssid with to be less than 0'''
         dest_callsign = 'DEST'
         dest_ssid = 1
         src_callsign = 'SRC'
@@ -85,11 +131,19 @@ class TestAx25(unittest.TestCase):
         payload = b'\x01\x02\x03'
 
         # Check if Ax25Error is raised for the invalid source ssid value
-        with self.assertRaisesRegex(Ax25Error, 'src_ssid must be between 0 and 15'):
-            generate_ax25_packet(dest_callsign, dest_ssid, src_callsign, invalid_src_ssid, control, pid, payload)
+        with self.assertRaises(Ax25Error):
+            generate_ax25_packet(
+                dest_callsign,
+                dest_ssid,
+                src_callsign,
+                invalid_src_ssid,
+                control,
+                pid,
+                payload
+            )
 
     def test_generate_ax25_invalid_payload(self):
-        #Set payload with a length greater than AX25_PAYLOAD_MAX_LEN
+        '''Set payload with a length greater than AX25_PAYLOAD_MAX_LEN'''
         dest_callsign = 'DEST'
         dest_ssid = 1
         src_callsign = 'SRC'
@@ -99,5 +153,13 @@ class TestAx25(unittest.TestCase):
         invalid_payload = b'\x00' * (AX25_PAYLOAD_MAX_LEN + 1)
 
         # Check if Ax25Error is raised for the invalid source ssid value
-        with self.assertRaisesRegex(Ax25Error, f'payload must be less than {AX25_PAYLOAD_MAX_LEN} bytes'):
-            generate_ax25_packet(dest_callsign, dest_ssid, src_callsign, src_ssid, control, pid, invalid_payload)
+        with self.assertRaises(Ax25Error):
+            generate_ax25_packet(
+                dest_callsign,
+                dest_ssid,
+                src_callsign,
+                src_ssid,
+                control,
+                pid,
+                invalid_payload
+            )
--- tests/protocols/test_edl.py
+++ tests/protocols/test_edl.py
@@ -1,9 +1,8 @@
 import unittest
-import unittest.mock as um
 
 from spacepackets.uslp.frame import TransferFrame, FrameType
+from oresat_c3.protocols.edl import EdlServer, EdlClient, EdlError
 
-from oresat_c3.protocols.edl import EdlServer, EdlClient, EdlError, crc16_bytes
 
 class TestEdl(unittest.TestCase):
 
@@ -26,110 +25,95 @@ class TestEdl(unittest.TestCase):
 
     def test_parse_packet_short_packet(self):
 
-        #Creating an instance of EdlServer with a dummy HMAC key and sequence number
+        # Creating an instance of EdlServer with a dummy HMAC key and sequence number
         hmac_key = b'\x00' * 128
         seq_num = 0
         server = EdlServer(hmac_key, seq_num)
 
-        #Preparing a packet that is shorter than the minimum length
+        # Preparing a packet that is shorter than the minimum length
         short_packet = b'\x00' * (server._TC_MIN_LEN - 1)
 
-        #Test if EdlError "EDL packet too short" exception is thrown
-        with self.assertRaisesRegex(EdlError, f'EDL packet too short: {server._TC_MIN_LEN - 1}'):
-            server._parse_packet(short_packet, src_dest = 0) #Using 0 as a dummy src_dest value
+        # Test if EdlError "EDL packet too short" exception is thrown
+        with self.assertRaises(EdlError):
+            server.parse_request(short_packet)
 
     def test_parse_packet_invalid_fecf(self):
 
-       hmac_key = b'\x00' * 128
-       seq_num = 0
-       payload = b'\x12\x34\x56'
+        hmac_key = b'\x00' * 128
+        seq_num = 0
+        payload = b'\x12\x34\x56'
 
-       #Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
-       server = EdlServer(hmac_key, seq_num)
-       client = EdlClient(hmac_key, seq_num)
+        # Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
+        server = EdlServer(hmac_key, seq_num)
+        client = EdlClient(hmac_key, seq_num)
 
-       #Generating a valid request ticket
-       req_packet = client.generate_request(payload)
+        # Generating a valid request ticket
+        req_packet = client.generate_request(payload)
 
-       #Modifying FECF so that it is Invalid
-       invalid_fecf_packet = req_packet[:-2] + b'\xFF\xFF'
+        # Modifying FECF so that it is Invalid
+        invalid_fecf_packet = req_packet[:-2] + b'\xFF\xFF'
 
-       #Checking if EdlError exception is raised for the invalid FECF
-       crc16_raw = invalid_fecf_packet[-2:]
-       crc16_raw_calc = crc16_bytes(invalid_fecf_packet[:-2])
-       with self.assertRaisesRegex(EdlError, f'invalid FECF: .* vs .*'):
-        server._parse_packet(invalid_fecf_packet, src_dest = 0)
+        # Checking if EdlError exception is raised for the invalid FECF
+        with self.assertRaises(EdlError):
+            server.parse_request(invalid_fecf_packet)
 
     def test_parse_packet_invalid_packet_or_frame_len(self):
-       hmac_key = b'\x00' * 128
-       seq_num = 0
-       payload = b'\x12\x34\x56'
+        hmac_key = b'\x00' * 128
+        seq_num = 0
+        payload = b'\x12\x34\x56'
 
-       #Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
-       server = EdlServer(hmac_key, seq_num)
-       client = EdlClient(hmac_key, seq_num)
+        # Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
+        server = EdlServer(hmac_key, seq_num)
+        client = EdlClient(hmac_key, seq_num)
 
-       #Generating a valid request packet
-       req_packet = client.generate_request(payload)
+        # Generating a valid request packet
+        req_packet = client.generate_request(payload)
 
-       #Modifying the packet length in the primary header so that it's invalid
-       invalid_packet_len = 255
-       invalid_packet = req_packet[:4] + invalid_packet_len.to_bytes(2, 'big') + req_packet[6:]
-       #mock_crc16_bytes.return_value = invalid_packet[-2:]
+        # Modifying the packet length in the primary header so that it's invalid
+        invalid_packet_len = 255
+        invalid_packet = req_packet[:4] + invalid_packet_len.to_bytes(2, 'big') + req_packet[6:]
 
-       #Checking if EdlError is raised for invalid packet or fram length
-       #Use patch as a context manager
-       #with um.patch("oresat_c3.protocols.edl.crc16_bytes") as mock_crc16_bytes:
-        #   mock_crc16_bytes.return_value = invalid_packet[-2:]##
-       with self.assertRaisesRegex(EdlError, 'USLP invalid packet or frame length'):
-        server._parse_packet(invalid_packet, src_dest = 0)
+        with self.assertRaises(EdlError):
+            server.parse_request(invalid_packet)
 
     def test_parse_packet_invalid_sequence_number(self):
         hmac_key = b'\x00' * 128
         seq_num = 0
         payload = b'\x12\x34\x56'
 
-        #Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
+        # Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
         server = EdlServer(hmac_key, seq_num)
         client = EdlClient(hmac_key, seq_num)
 
-        #Generating a valid request packet
+        # Generating a valid request packet
         req_packet = client.generate_request(payload)
 
-        #Use patch as a context manager
-        with um.patch("oresat_c3.protocols.edl.TransferFrame.unpack") as mock_unpack:
-            #Create a mock frame with an invalid insert_zone value
-            mock_frame = TransferFrame(req_packet, FrameType.VARIABLE)
-            mock_frame.insert_zone = server.sequence_number_bytes + bytes(1)
+        # Create a frame with an invalid insert_zone value
+        frame = TransferFrame.unpack(req_packet, FrameType.VARIABLE, server.FRAME_PROPS)
+        frame.insert_zone = server.sequence_number_bytes + bytes(1)
+        invalid_packet = frame.pack(frame_type=FrameType.VARIABLE)
 
-            #Set the return value of the mocked unpack method
-            mock_unpack.return_value = mock_frame
-
-            #Checking if the EdlError is raised for invalid sequence number
-            with self.assertRaisesRegex(EdlError, f'invalid sequence number: .* vs .*'):
-                server._parse_packet(req_packet, src_dest=0)
+        # Checking if the EdlError is raised for invalid sequence number
+        with self.assertRaises(EdlError):
+            server.parse_request(invalid_packet)
 
     def test_parse_packet_invalid_HMAC(self):
         hmac_key = b'\x00' * 128
         seq_num = 0
         payload = b'\x12\x34\x56'
 
-        #Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
+        # Creating an instance of EdlServer and EdlClient with a dummy HMAC key and sequence number
         server = EdlServer(hmac_key, seq_num)
         client = EdlClient(hmac_key, seq_num)
 
-        #Generating a valid request packet
+        # Generating a valid request packet
         req_packet = client.generate_request(payload)
 
-        #Use patch as a context manager
-        with um.patch("oresat_c3.protocols.edl.TransferFrame.unpack") as mock_unpack:
-            #Create a mock frame with a valid insert_zone value and an invalid HMAC
-            mock_frame = TransferFrame(req_packet, FrameType.VARIABLE)
-            mock_frame.insert_zone = server.sequence_number_bytes
-            mock_frame.tfdf.tfdz= payload + b'\x00' * server.HMAC_LEN
+        # Create a frame with a valid insert_zone value and an invalid HMAC
+        frame = TransferFrame.unpack(req_packet, FrameType.VARIABLE, server.FRAME_PROPS)
+        frame.insert_zone = server.sequence_number_bytes
+        frame.tfdf.tfdz = payload + b'\x00' * server.HMAC_LEN
+        invalid_packet = frame.pack(frame_type=FrameType.VARIABLE)
 
-            #Set the return value of the mocked unpack method
-            mock_unpack.return_value = mock_frame
-
-            with self.assertRaisesRegex(EdlError,  f'invalid HMAC .* vs .*'):
-                server._parse_packet(req_packet, src_dest=0)
+        with self.assertRaises(EdlError):
+            server.parse_request(invalid_packet)

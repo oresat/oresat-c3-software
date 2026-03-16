47647176898ba70c1bf2ffa725557a0e93d6133b
Andrew Stanton <81195290+sneakyaardvark@users.noreply.github.com>
2026-03-16 14:39:46 -0700

    test: reaction wheel test service

--- oresat_c3/__main__.py
+++ oresat_c3/__main__.py
@@ -20,6 +20,7 @@ from olaf import (
 )
 
 from oresat_c3.services.adcs_manager import ADCSManager
+from oresat_c3.services.rw_test import ReactionWheelTest
 from oresat_c3.subsystems.adcs.config import build_config
 from . import C3State, __version__
 from .protocols.cachestore import CacheStore
@@ -135,15 +136,17 @@ def main():
     beacon_service = BeaconService(config.beacon_def, radios_service)
     node_mgr_service = NodeManagerService(config.cards, mock_hw=mock_hw)
     edl_service = EdlService(app.node, radios_service, node_mgr_service, beacon_service)
-    adcs_config = build_config()
-    adcs_mgr_service = ADCSManager(adcs_config, mock_hw)
+    rw_test = ReactionWheelTest(mock_hw)
+    # adcs_config = build_config()
+    # adcs_mgr_service = ADCSManager(adcs_config, mock_hw)
 
     app.add_service(state_service)  # add state first to restore state from F-RAM
     app.add_service(radios_service)
     app.add_service(beacon_service)
     app.add_service(edl_service)
     app.add_service(node_mgr_service)
-    app.add_service(adcs_mgr_service)
+    app.add_service(rw_test)
+    # app.add_service(adcs_mgr_service)
 
     for file_name in os.listdir(f"{path}/templates"):
         rest_api.add_template(f"{path}/templates/{file_name}")

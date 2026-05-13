db6c8d8528384c9300c636efdfa5aedb44a89892
Andrew Stanton <81195290+sneakyaardvark@users.noreply.github.com>
2026-05-12 17:22:06 -0700

    refactor: move frame creation to uslp.py

--- oresat_c3/protocols/uslp.py
+++ oresat_c3/protocols/uslp.py
@@ -1,7 +1,18 @@
+from typing import Optional
+
+from spacepackets.uslp import (
+    BypassSequenceControlFlag,
+    PrimaryHeader,
+    ProtocolCommandFlag,
+    SourceOrDestField,
+)
 from spacepackets.uslp.defs import UslpInvalidRawPacketOrFrameLenError
 from spacepackets.uslp.frame import (
     FrameType,
+    TfdzConstructionRules,
     TransferFrame,
+    TransferFrameDataField,
+    UslpProtocolIdentifier,
     VarFrameProperties,
 )
 
@@ -66,3 +77,67 @@ def unpack_frame(raw: bytes) -> TransferFrame:
         raise UslpInvalidSpacecraftIdError
 
     return frame
+
+
+def make_frame(
+    payload: bytes,
+    vcid: int,
+    src_dest: SourceOrDestField,
+    vcf_count: Optional[int] = None,
+    control_word: Optional[bytes] = None,
+    insert_zone: Optional[bytes] = None,
+) -> TransferFrame:
+    """Create and pack a USLP
+
+    Parameters
+    ----------
+    payload
+        The pre-packed payload.
+    vcid
+        The Virtual Channel Identifier of the frame.
+    src_dest
+        The Source or Destination identifier.
+    vcf_count
+        The Virtual Channel Frame count. If None, the VCF length is to 0 and no count is specified.
+    control_word
+        The CLCW, if any, to pack in the frame.
+    insert_zone
+        The insert zone data, if any.
+
+    Returns
+    -------
+    TransferFrame
+        The constructed Transfer Frame.
+    """
+
+    tfdf = TransferFrameDataField(
+        tfdz_cnstr_rules=TfdzConstructionRules.VpNoSegmentation,
+        uslp_ident=UslpProtocolIdentifier.USER_DEFINED_OCTET_STREAM,
+        tfdz=payload,
+    )
+
+    # USLP transfer frame total length - 1
+    frame_len = len(payload) + PRIMARY_HEADER_LEN + DFH_LEN + FECF_LEN - 1
+    if insert_zone:
+        frame_len += len(insert_zone)
+
+    has_clcw = bool(control_word)
+    if has_clcw:
+        frame_len += len(control_word)
+
+    frame_header = PrimaryHeader(
+        scid=SPACECRAFT_ID,
+        map_id=0,
+        vcid=vcid,
+        src_dest=src_dest,
+        frame_len=frame_len,
+        vcf_count_len=bool(vcf_count),
+        vcf_count=vcf_count,
+        op_ctrl_flag=has_clcw,
+        prot_ctrl_cmd_flag=ProtocolCommandFlag.USER_DATA,
+        bypass_seq_ctrl_flag=BypassSequenceControlFlag.SEQ_CTRLD_QOS,
+    )
+
+    return TransferFrame(
+        header=frame_header, tfdf=tfdf, op_ctrl_field=control_word, insert_zone=insert_zone
+    )

a12313d6111382b9e2b2284c26731f2409282c04
Andrew Stanton <81195290+sneakyaardvark@users.noreply.github.com>
2026-05-14 11:05:28 -0700

    feat: initial FOP-1 parameters

---
+++ oresat_c3/protocols/cop1/fop.py
@@ -0,0 +1,48 @@
+from enum import unique, Enum
+from typing import Optional
+
+from spacepackets.uslp import TransferFrame
+
+from .common import CopService
+
+
+@unique
+class FopState(Enum):
+    """The state of FOP-1
+
+    CCSDS 232.1-B-2 § 5.1.2
+    """
+    ACTIVE = 1
+    RETRANSMIT_NO_WAIT = 2
+    RETRANSMIT_WITH_WAIT = 3
+    INITIALIZING_NO_BC = 4
+    INITIALIZING_WITH_BC = 5
+    INITIAL = 6
+
+
+class Fop1(CopService):
+    """Frame Operation Procedure-1 (FOP-1), CCSDS 323.1-B-2"""
+    def __init__(
+        self,
+        k: int = 10,
+    ) -> None:
+        super().__init__()
+        self.state: FopState = FopState.INITIAL
+        # TRANSMITTER_FRAME_SEQUENCE_NUMBER, V(S)
+        self.v_s: int = 0
+        self.wait_queue: Optional[TransferFrame] = None
+        self.send_queue: Optional[TransferFrame] = None
+        self.to_be_retransmitted: bool
+        self.ad_out: bool
+        self.bd_out: bool
+        self.bc_out: bool
+        # Expected_Acknowledgement_Frame_Sequence_Number
+        self.nn_r: int = 0
+        self.timer_initial_value: int = 0
+        self.transmission_limit: int
+        self.transmission_count: int
+        if k < 1 or k >= 256:
+            raise ValueError("k must be between 1 and 256")
+        self.sliding_window_width: int  # 'K'
+        self.timeout_type: int
+        self.suspend_state: int

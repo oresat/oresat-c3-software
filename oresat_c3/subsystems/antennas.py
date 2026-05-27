607007a387fc90f3684d884318a17ce696c99847
Hunter-McClellan <huntermcclellan0@gmail.com>
2026-05-27 15:59:24 -0600

    Added untested implementation

--- oresat_c3/subsystems/antennas.py
+++ oresat_c3/subsystems/antennas.py
@@ -2,12 +2,23 @@
 
 from time import sleep
 
-from olaf import GPIO_IN, GPIO_OUT, Adc, Gpio
+from olaf import GPIO_IN, GPIO_OUT, Adc, Gpio, logger
+
+from ..drivers.max7310 import Max7310, Max7310Error, MockMax7310
 
 
 class Antennas:
     """Antennas subsystem."""
 
+    _TIMEOUT_CONFIG = 1
+
+    _I2C_BUS_NUM = 2 #reimplementation of whats in node_manager. Bad and should be fixed.	
+
+    _READ_ANT_PIN = 0
+    _FIRE_ANT_1_PIN = 1
+    _FIRE_ANT_2_PIN = 2
+    _TEST_ANT_PIN = 3
+    
     def __init__(self, mock: bool = False):
         """
         Parameters
@@ -16,16 +27,61 @@ class Antennas:
             Mock the hardware.
         """
 
-        self._gpio_monopole_1 = Gpio("FIRE_ANTENNAS_1", mock)
-        self._gpio_monopole_2 = Gpio("FIRE_ANTENNAS_2", mock)
-        self._gpio_helical_1 = Gpio("FIRE_HELICAL_1", mock)
-        self._gpio_helical_2 = Gpio("FIRE_HELICAL_2", mock)
+        if not mock:
+            self._pz_end_max7310 = Max7310(_I2C_BUS_NUM, 0x14)
+            self._mz_end_max7310 = Max7310(_I2C_BUS_NUM, 0x15)
+            self._mz_mid_max7310 = Max7310(_I2C_BUS_NUM, 0x16)
+        else:
+            self._pz_end_max7310 = Max7310(_I2C_BUS_NUM, 0x14, 0)
+            self._mz_end_max7310 = Max7310(_I2C_BUS_NUM, 0x15, 0)
+            self._mz_mid_max7310 = Max7310(_I2C_BUS_NUM, 0x16, 0)
 
-        self._gpio_test_monopole = Gpio("TEST_ANTENNAS", mock)
-        self._gpio_test_helical = Gpio("TEST_HELICAL", mock)
+        self._live_inputs = 1 << self._READ_ANT_PIN & 1 << self._TEST_ANT_PIN
+        self._safe_inputs = self._live_inputs & 1 << self._FIRE_ANT_1_PIN & 1 << self._FIRE_ANT_2_PIN
+        self.probe_cards()
 
-        self._adc_monopole = Adc(4, mock)
-        self._adc_helical = Adc(5, mock)
+
+    def probe_cards(self):
+        """
+            Looks for the endcards on the OPD. any that end cards that cannot be found or fail are set to None
+        """
+        try:
+            if self._pz_end_max7310.is_valid:
+                logger.info("Found plus z end card.")
+                self._max7310.configure(0, 0, self._safe_inputs, self._TIMEOUT_CONFIG)
+            else:
+                logger.info("Could not find plus z end card.")
+                self._pz_end_max7310 = None
+        except Max7310Error as e:
+            logger.error(f"MAX7310 error: {e}")
+            logger.info(f"Failed to setup plus z end card with error.")
+            self._pz_end_max7310 = None
+
+        try:
+            if self._mz_end_max7310.is_valid:
+                logger.info("Found minus z end card.")
+                self._max7310.configure(0, 0, self._safe_inputs, self._TIMEOUT_CONFIG)
+            else:
+                logger.info("Could not find minus z end card.")
+                self._mz_end_max7310 = None
+        except Max7310Error as e:
+            logger.error(f"MAX7310 error: {e}")
+            logger.info(f"Failed to setup minus z end card with error.")
+            self._mz_end_max7310 = None
+
+        try:
+            if self._mz_mid_max7310.is_valid:
+                logger.info("Found minus z mid card.")
+                self._max7310.configure(0, 0, self._safe_inputs, self._TIMEOUT_CONFIG)
+            else:
+                logger.info("Could not find minus z mid card.")
+                self._mz_mid_max7310 = None
+        except Max7310Error as e:
+            logger.error(f"MAX7310 error: {e}")
+            logger.info(f"Failed to setup minus z mid card with error.")
+            self._mz_mid_max7310 = None
+
+
 
     def deploy(self, timeout: int, delay_between: int):
         """
@@ -41,92 +97,63 @@ class Antennas:
             Delay between the monopole and helical deployments.
         """
 
-        self.deploy_monopole(timeout)
-        sleep(delay_between)
-        self.deploy_helical(timeout)
+        if self._pz_end_max7310 != None:
+            logger.info("Attempting pos z end card firing.")
+            self.deploy(timeout, self._pz_end_max7310)
+            sleep(delay_between)
+        if self._mz_end_max7310 != None:
+            logger.info("Attempting minus z end card firing.")
+            self.deploy(timeout, self._mz_end_max7310)
+            sleep(delay_between)
+        if self._mz_mid_max7310 != None:
+            logger.info("Attempting minus z mid card firing.")
+            self.deploy(timeout, self._mz_mid_max7310)
 
-    def deploy_helical(self, timeout: int):
+    def deploy(self, timeout: int, _max7310: Max7310):
         """
-        Deploy only the helical.
+        Deploy using the specified MAX7310 chip.
 
         Parameters
         ----------
         timeout: int
             How long the gpio lines are set high.
+        max: Max7310
+            The MAX7310 to try to deploy with
         """
 
-        self._gpio_helical_1.mode = GPIO_OUT
-        self._gpio_helical_2.mode = GPIO_OUT
+        try:
+            _max7310.configure(0, 0, self._live_inputs, self._TIMEOUT_CONFIG)
+            _max7310.output_set(self._FIRE_ANT_1_PIN)
+            _max7310.output_set(self._FIRE_ANT_2_PIN)
 
-        self._gpio_helical_1.high()
-        self._gpio_helical_2.high()
-        sleep(timeout)
-        self._gpio_helical_1.low()
-        self._gpio_helical_2.low()
+            sleep(timeout)
 
-        self._gpio_helical_1.mode = GPIO_IN
-        self._gpio_helical_2.mode = GPIO_IN
+            _max7310.output_clear(self._FIRE_ANT_1_PIN)
+            _max7310.output_clear(self._FIRE_ANT_2_PIN)
+            _max7310.configure(0, 0, self._safe_inputs, self._TIMEOUT_CONFIG)
+        except Max7310Error as e:
+            logger.error(f"MAX7310 error: {e}")
+            logger.info(f"Tried and failed to fire deployer.")
 
-    def deploy_monopole(self, timeout: int):
-        """
-        Deploy only the monopole.
 
-        Parameters
-        ----------
-        timeout: int
-            How long the gpio lines are set high.
-        """
+    # Commenting this out for now. We may want to reimplement this later.
+    # def is_helical_good(self, good_threshold: int) -> bool:
+    #     """
+    #     Test the helical resistor.
 
-        self._gpio_monopole_1.mode = GPIO_OUT
-        self._gpio_monopole_2.mode = GPIO_OUT
+    #     Parameters
+    #     ----------
+    #     good_threshold: int
+    #         The good threshold (anything above this value is good) in millivolts for
+    #         testing an antenna.
 
-        self._gpio_monopole_1.high()
-        self._gpio_monopole_2.high()
-        sleep(timeout)
-        self._gpio_monopole_1.low()
-        self._gpio_monopole_2.low()
+    #     Returns
+    #     -------
+    #     bool
+    #         Helical is good.
+    #     """
 
-        self._gpio_monopole_1.mode = GPIO_IN
-        self._gpio_monopole_2.mode = GPIO_IN
-
-    def is_helical_good(self, good_threshold: int) -> bool:
-        """
-        Test the helical resistor.
-
-        Parameters
-        ----------
-        good_threshold: int
-            The good threshold (anything above this value is good) in millivolts for
-            testing an antenna.
-
-        Returns
-        -------
-        bool
-            Helical is good.
-        """
-
-        self._gpio_test_helical.high()
-        value = self._adc_helical.value
-        self._gpio_test_helical.low()
-        return value >= good_threshold
-
-    def is_monopole_good(self, good_threshold: int) -> bool:
-        """
-        Test the monopole resistor.
-
-        Parameters
-        ----------
-        good_threshold: int
-            The good threshold (anything above this value is good) in millivolts for
-            testing an antenna.
-
-        Returns
-        -------
-        bool
-            Monopole is good.
-        """
-
-        self._gpio_test_monopole.high()
-        value = self._adc_monopole.value
-        self._gpio_test_monopole.low()
-        return value >= good_threshold
+    #     self._gpio_test_helical.high()
+    #     value = self._adc_helical.value
+    #     self._gpio_test_helical.low()
+    #     return value >= good_threshold

43b65412d1d1256be50a60eae679c432e538b930
Andrew Stanton <81195290+sneakyaardvark@users.noreply.github.com>
2026-06-25 11:17:05 -0700

    wip: fop higher and lower procedures

--- oresat_c3/services/cop_manager.py
+++ oresat_c3/services/cop_manager.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from queue import Empty, SimpleQueue
 from typing import Optional
 
@@ -8,11 +9,34 @@ from ccsds_cop.cop_1.farm import (
     FduArrivedIndication,
     ValidFrameArrivedIndication,
 )
+from ccsds_cop.cop_1.fop import (
+    AbortRequest,
+    AsyncNotification,
+    AsyncNotificationType,
+    DirectiveNotification,
+    Fop1,
+    NotificationType,
+    TransferNotification,
+    TransmitRequestForFrame,
+)
 from olaf import Service, logger
-from spacepackets.uslp import TransferFrame
+from spacepackets.uslp import SourceOrDestField, TransferFrame
+from uslp import make_frame
 
 from ..protocols.edl_packet import EdlVcid
 
+
+@dataclass
+class FopInstance:
+    service: Fop1
+    recv_queue: SimpleQueue[TransferFrame] = field(default_factory=SimpleQueue)
+    requests: dict[int, bool] = field(default_factory=dict)
+    _next_rid = 0
+
+    def next_rid(self) -> int:
+        rid = self._next_rid
+        self._next_rid += 1
+        return rid
 
 class CopManagerService(Service):
     """COP-1 Services Manager
@@ -23,6 +47,7 @@ class CopManagerService(Service):
     def __init__(self) -> None:
         super().__init__()
         self._farms: dict[EdlVcid, tuple[CopService, SimpleQueue[TransferFrame]]] = {}
+        self._fops: dict[EdlVcid, FopInstance] = {}
         self.recv_queue: SimpleQueue[TransferFrame] = SimpleQueue()
 
     def on_loop(self) -> None:
@@ -57,6 +82,56 @@ class CopManagerService(Service):
                     hi.buffer_release.set()
             except IndexError:
                 continue
+
+    def _process_fop_lower(self) -> None:
+        for srv, q in self._fops.values():
+            i = srv.interface.to_lower.pop()
+            if isinstance(i, TransmitRequestForFrame):
+                fr = make_frame(
+                    payload=i.tfdf,
+                    vcid=i.gvcid.vcid,
+                    src_dest=SourceOrDestField.SOURCE,
+                    vcf_count=i.v_s,
+                )
+                q.put_nowait(fr)
+            elif isinstance(i, AbortRequest):
+                # TODO: if anything is being processed for the GVCID, abort them
+                pass
+            else:
+                logger.error(f"Unknown FOP-1 Lower Procedures request of type {type(i)}")
+
+    def _process_fop_higher(self) -> None:
+        for instance in self._fops.values():
+            try:
+                i = instance.service.interface.to_higher.pop()
+            except IndexError:
+                continue
+            if isinstance(i, DirectiveNotification):
+                if i.notification_type == NotificationType.ACCEPT:
+                    instance.requests[i.request_id] = True
+                elif i.notification_type == NotificationType.REJECT:
+                    # TODO: notify the user
+                    del instance.requests[i.request_id]
+                elif i.notification_type == NotificationType.POSITIVE_CONFIRM:
+                    # TODO: notify user of completion
+                    del instance.requests[i.request_id]
+                elif i.notification_type == NotificationType.NEGATIVE_CONFIRM:
+                    # TODO: notify the user, note: an alert will always be received before
+                    #  NEGATIVE_CONFIRM
+                    del instance.requests[i.request_id]
+            elif isinstance(i, AsyncNotification):
+                if i.notification_type == AsyncNotificationType.ALERT:
+                    pass  # TODO: notify user
+            elif isinstance(i, TransferNotification):
+                if i.notification_type == NotificationType.ACCEPT:
+                    pass
+                elif i.notification_type == NotificationType.REJECT:
+                    pass
+                elif i.notification_type == NotificationType.POSITIVE_CONFIRM:
+                    pass
+                elif i.notification_type == NotificationType.NEGATIVE_CONFIRM:
+                    pass
+
 
     def create_farm_service(self, vcid: EdlVcid) -> SimpleQueue[TransferFrame]:
         logger.info(f"Creating FARM-1 Service for VCID {vcid}")
